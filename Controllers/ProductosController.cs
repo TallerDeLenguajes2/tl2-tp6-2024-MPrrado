8c6718d9e1d5525e84eef775b8f94e4ef7ca4cb1
Prado Matias Santiago <matias14prado@gmail.com>
2024-11-11 21:47:24 -0300

    implementacion del controlador de productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,3 +1,4 @@
+using EspacioProductos;
 using EspacioRepositorios;
 using Microsoft.AspNetCore.Mvc;
 public class ProductosController : Controller
@@ -15,8 +16,36 @@ public class ProductosController : Controller
     }
 
     [HttpGet]
+    public IActionResult AltaProducto()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AltaProducto(Producto producto)
+    {
+        productoRepository.AltaProducto(producto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
     public IActionResult ModificarProducto(int idProducto)
     {
         return View(productoRepository.GetProducto(idProducto));
     }
+
+    [HttpPost]
+    public IActionResult ModificarProducto(Producto producto)
+    {
+        productoRepository.ModificarProducto(producto.IdProducto, producto);
+        return RedirectToAction("Index");
+    }
+
+    // [HttpDelete] porque no puedo dejar este atributo, si lo pongo me sale error  405
+    public IActionResult EliminarProducto(int idProducto)
+    {
+        productoRepository.EliminarProducto(idProducto);
+        return RedirectToAction("Index");
+    }
+
 }
